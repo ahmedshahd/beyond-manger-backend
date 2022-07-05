db0f62dc6e63804cd2550959833bc05c330df996
Ali Gamal <ali.gamal@beyond-solution.com>
2022-07-05 14:55:45 +0200

    NF: update beyond manager schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { AppService } from './app.service';
 import { BeyondManagerModule } from './beyond-manager/beyond-manager.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import * as moment from 'moment-timezone';
 
-// const mocks = {
-//   Int: () => 6,
-//   Float: () => 22.1,
-//   String: () => 'Hello',
-// };
+const mocks = {
+  Int: () => Math.floor(Math.random() * (100 - 1)) + 1,
+  Float: () => Math.random() * (100 - 1) + 1,
+  String: () => (Math.random() + 1).toString(36).substring(7),
+  Void: () => null,
+  DateTime: ()=> moment(1448841600000).tz("Africa/Cairo").format(),
+};
 
 @Module({
   imports: [    
@@ -24,7 +27,7 @@ import { join } from 'path';
         outputAs: 'class',
       },
     
-      mocks:true,
+      mocks,
     }),
     BeyondManagerModule,
     
--- src/graphql.ts
+++ src/graphql.ts
@@ -8,7 +8,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export class BeyondManager {
-    id: string;
+    id: number;
     first_name: string;
     last_name: string;
     email: string;
@@ -28,10 +28,17 @@ export class BeyondManager {
 }
 
 export abstract class IQuery {
-    abstract GetBeyondManagers(): BeyondManager[] | Promise<BeyondManager[]>;
+    abstract beyondManagers(limit?: Nullable<number>, offset?: Nullable<number>): BeyondManager[] | Promise<BeyondManager[]>;
 
-    abstract GetBeyondManager(id: string): Nullable<BeyondManager> | Promise<Nullable<BeyondManager>>;
+    abstract beyondManager(id: number): Nullable<BeyondManager> | Promise<Nullable<BeyondManager>>;
+
+    abstract beyondManagerChangePassword(old_password?: Nullable<string>, new_password?: Nullable<string>, confirm_password?: Nullable<string>, isFirstReset?: Nullable<boolean>): Nullable<Void> | Promise<Nullable<Void>>;
+
+    abstract beyondManagerRequestForgetPassword(email?: Nullable<string>): Nullable<Void> | Promise<Nullable<Void>>;
+
+    abstract beyondManagerVerifyForgetPassword(email?: Nullable<string>, temporary_password?: Nullable<string>, new_password?: Nullable<string>, confirm_password?: Nullable<string>): Nullable<Void> | Promise<Nullable<Void>>;
 }
 
 export type DateTime = any;
+export type Void = any;
 type Nullable<T> = T | null;
